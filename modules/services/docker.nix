2b42a584f0bc1058fa8b2882a428452a299a8b1e
Paw Møller <pawsen@gmail.com>
2022-08-05 18:41:50 +0200

    howto: enable ipv6 in docker. But don't do it!

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -27,6 +27,15 @@ in {
         enable = true;
         autoPrune.enable = true;
         enableOnBoot = mkDefault false;
+        # HOWTO: enable ipv6 in docker. But don't do it!
+        # https://medium.com/@skleeschulte/how-to-enable-ipv6-for-docker-containers-on-ubuntu-18-04-c68394a219a2
+        # extraOptions = "--ipv6 --fixed-cidr-v6 2001:db8:1::/64";
+        # Then enable IPv6 in the docker-compose.yaml file by adding a networks section
+        # Check that IPv6 is enabled with dk network inspect docker0 (the base)
+        # and the container specific network
+        # To enable IPv6 internet access from containers, enable NAT for the private Docker subnet on the host:
+        # : ip6tables -t nat -A POSTROUTING -s 2001:db8:1::/64 ! -o docker0 -j MASQUERADE
+        # (and find a way to make it persistent)
         # listenOptions = [];
       };
     };
