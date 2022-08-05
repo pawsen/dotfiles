672983fcc1106a966a34f31f91b2d2c6a7bc1f0d
Paw Møller <pawsen@gmail.com>
2022-08-05 18:32:47 +0200

    more emacs aliases

--- config/emacs/aliases.zsh
+++ config/emacs/aliases.zsh
@@ -1,6 +1,13 @@
 #!/usr/bin/env zsh
 
-e()     { pgrep emacs && emacsclient -n "$@" || emacs -nw "$@" }
+e()     { pgrep emacs && emacsclient -nw "$@" || emacs -nw "$@" }
 ediff() { emacs -nw --eval "(ediff-files \"$1\" \"$2\")"; }
 eman()  { emacs -nw --eval "(switch-to-buffer (man \"$1\"))"; }
 ekill() { emacsclient --eval '(kill-emacs)'; }
+# always create a new X frame
+ef()    { pgrep emacs && emacsclient -c -n "$@" || emacs "$@" }
+alias et='e'
+
+# edit file with root privs
+alias efs="SUDO_EDITOR=\"emacsclient -c -a emacs\" sudoedit"
+alias ets="SUDO_EDITOR=\"emacsclient -t -a emacs\" sudoedit"
