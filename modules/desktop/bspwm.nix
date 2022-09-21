ac89a540c74df52d0d7be42afd28ff655c34f46f
Paw Møller <pawsen@gmail.com>
2022-09-21 11:55:36 +0200

    use https://github.com/mattydebie/bitwarden-rofi
    instead of hlissners ruby-script

--- modules/desktop/bspwm.nix
+++ modules/desktop/bspwm.nix
@@ -24,6 +24,7 @@ in {
         pulseSupport = true;
         nlSupport = true;
       })
+      keyutils    # provide keyctl, used for rofi-bwmenu
     ];
 
     programs = {
@@ -52,6 +53,28 @@ in {
       serviceConfig.Restart = "always";
       serviceConfig.RestartSec = 2;
       serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst";
+    };
+
+    # keyctl fix,
+    # https://discourse.nixos.org/t/keyctl-read-alloc-permission-denied/8667/4
+    # links the UID0(@u) to the current session(@s) in the kernel keyring
+    # https://mjg59.dreamwidth.org/37333.html
+    systemd.user.services."keyctl_fix" = {
+      enable = true;
+      description = "keyctl fix";
+      wantedBy = [ "default.target" ];
+      # make keyctl available to the shell
+      path = [ pkgs.keyutils ];
+      serviceConfig = {
+        Type = "oneshot";
+        # only set user/group for system units. Newer user units!
+        # User = config.user.name;
+        # Group = config.user.group;
+      };
+      script = ''
+           command -v keyctl >/dev/null 2>&1 || { echo >&2 "I require keyctl but it's not available."; }
+           keyctl link @u @s
+           '';
     };
 
     # link recursively so other modules can link files in their folders
